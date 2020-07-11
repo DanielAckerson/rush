a694338c978ba870afb7a2b8013447592d0975aa
Daniel Ackerson <dackerson0@gmail.com>
2020-07-11 14:53:14 -0400

    use nix crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use nix::unistd::{fork, ForkResult};
+
 fn main() {
-    println!("Hello, world!");
+    match fork() {
+        Ok(ForkResult::Parent { child, ..}) => {
+            println!("Continuing execution in parent process, new child has pid: {}", child);
+        }
+        Ok(ForkResult::Child) => println!("I'm a new child process"),
+        Err(_) => println!("Fork failed"),
+    }
 }
