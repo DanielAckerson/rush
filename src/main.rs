c474c22a8a942b8acf6aad4903525d3b2e2c109d
Daniel Ackerson <dackerson0@gmail.com>
2020-07-28 20:17:36 -0400

    parse quoted args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+#[macro_use]
+extern crate lazy_static;
+
+mod parse;
+
 use std::io::{self, Write};
 use std::process::{self, Command};
 
+
+// TODO: instead of working directly with path and args after parsing, generate a 
+// graph of tasks to execute and then execute them
 fn main() {
     let mut input = String::new();
     loop {
@@ -9,7 +17,7 @@ fn main() {
         match io::stdin().read_line(&mut input) {
             Ok(0) => break,
             Ok(_) => if let Some((path, args)) = eval_input(input.trim()) {
-                exec_input(path, args);
+                exec_input(&path, args.iter().map(AsRef::as_ref).collect());
             },
             Err(_) => process::exit(1),
         }
@@ -19,12 +27,22 @@ fn main() {
 }
 
 
-fn eval_input(input: &str) -> Option<(&str, Vec<&str>)> {
-    let mut expr_iter = input.split_whitespace();
-    if let Some(path) = expr_iter.next() {
-        Some((path, expr_iter.collect()))
-    } else {
-        None
+fn eval_input(input: &str) -> Option<(String, Vec<String>)> {
+    match parse::parse(input) {
+        Ok(mut expr) => {
+            let args = expr
+                .split_off(1)
+                .iter()
+                .map(|x| x.unwrap())
+                .collect();
+            let path = expr[0].unwrap();
+
+            Some((path, args))
+        },
+        Err(msg) => {
+            eprintln!("error: {}", msg);
+            None
+        },
     }
 }
 
